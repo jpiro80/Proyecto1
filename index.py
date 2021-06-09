ef684c2d2532132b7d89328f593ca3b591bf84a5
jpiro80 <85123589+jpiro80@users.noreply.github.com>
2021-06-09 13:27:35 -0300

    Add files via upload

--- index.py
+++ index.py
@@ -9,25 +9,25 @@ class Product:
 
     def __init__(self, window):
         self.wind = window
-        self.wind.title('Products Application')
+        self.wind.title('Pagos del mes')
 
         # Creating a Frame Container
-        frame = LabelFrame(self.wind, text = 'Register A new Product') 
+        frame = LabelFrame(self.wind, text = 'Agregar nuevo impuesto') 
         frame.grid(row = 0, column = 0, columnspan = 3, pady = 20)   
 
         # Name Input
-        Label(frame, text = 'Name: ').grid(row = 1, column = 0)
+        Label(frame, text = 'Impuesto: ').grid(row = 1, column = 0)
         self.name = Entry(frame)
         self.name.focus()
         self.name.grid(row = 1, column = 1)
 
         # Price Input
-        Label(frame, text = 'Price: ').grid(row = 2, column = 0)
+        Label(frame, text = 'Importe: ').grid(row = 2, column = 0)
         self.price = Entry(frame)
         self.price.grid(row = 2, column = 1)
 
         # Button Add Product
-        ttk.Button(frame, text = 'Save Product', command = self.add_product).grid(row = 3, columnspan = 2, sticky = W + E)
+        ttk.Button(frame, text = 'Guardar cambios', command = self.add_product).grid(row = 3, columnspan = 2, sticky = W + E)
 
         # Output Messages
         self.message = Label(text = '', fg = 'red')
@@ -36,12 +36,12 @@ class Product:
         # Table
         self.tree = ttk.Treeview(height = 10, columns = 2)
         self.tree.grid(row = 4, column = 0, columnspan = 2)
-        self.tree.heading('#0', text = 'Name', anchor = CENTER)
-        self.tree.heading('#1', text = 'Price', anchor = CENTER)
+        self.tree.heading('#0', text = 'Impuesto', anchor = CENTER)
+        self.tree.heading('#1', text = 'Importe', anchor = CENTER)
 
         # Buttons
-        ttk.Button(text = 'DELETE', command = self.delete_product).grid(row = 5, column = 0, sticky = W + E)
-        ttk.Button(text = 'EDIT', command = self.edit_product).grid(row = 5, column = 1, sticky = W + E)
+        ttk.Button(text = 'BORRAR', command = self.delete_product).grid(row = 5, column = 0, sticky = W + E)
+        ttk.Button(text = 'EDITAR', command = self.edit_product).grid(row = 5, column = 1, sticky = W + E)
 
         # Filling the Row
         self.get_products()
